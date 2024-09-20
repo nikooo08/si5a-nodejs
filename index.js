a0a569720bc349d2ecb50346934788f72d6c4a6d
nikooo08 <nikocrysostomus_2226240010@mhs.mdp.ac.id>
2024-09-20 11:01:55 +0700

    updatee

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const http = require('http');
 
 // buat server
 const server = http.createServer( (req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Selamat pagiii');
+    // res.writeHead(200, {'Content-Type': 'text/html'});
+    // res.write('Selamat pagiii');
+
+    res.writeHead(200, {'Content-Type': 'texapplication/json'});
+    res.write(JSON.stringify({
+        'message'   : 'Selamat Pagiii',
+        'status'    : 'succes'
+    }));
     res.end();
 });
 
